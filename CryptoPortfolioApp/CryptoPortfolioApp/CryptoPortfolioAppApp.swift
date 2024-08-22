840ed9fa8fe1780a0200661431bf4fa4a010ff82
Rubio45 <aleshdiaz45@gmail.com>
2024-08-21 18:54:05 -0600

    CryptoPortfolioApp

---
+++ CryptoPortfolioApp/CryptoPortfolioApp/CryptoPortfolioAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoPortfolioAppApp.swift
+//  CryptoPortfolioApp
+//
+//  Created by Alex Diaz on 21/8/24.
+//
+
+import SwiftUI
+
+@main
+struct CryptoPortfolioAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
